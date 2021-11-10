7aedbb45dcb32f3d73122819c5fc3b7240069368
Rochelle Canale <chelle@cbo.me>
2021-11-10 13:15:17 +0800

    Topic 2.3

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="sidebar" class="flex-1">
+        Render Contacts
+        <List :items="contacts" itemKey="name" type="contact" />
+    </div>
+</template>
+
+<script>
+import List from '@/components/general/List'
+export default {
+    name: "Sidebar",
+    components: {
+        List
+    },
+    data() {
+        return {
+            contacts: [
+                { id: 1, name: "John Doe" }, 
+                { id: 2, name: "Jane Doe" }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/general/List.vue
@@ -0,0 +1,47 @@
+<template>
+    <ul>
+        <Item 
+        v-for="item in allContacts"
+        :key="item.id"
+        :class="type">
+            {{ item[itemKey] }}
+        </Item>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "List",
+        components: {
+            Item: {
+                name: "Item",
+                render(createElement) {
+                    return createElement('li', this.$slots.default)
+                }
+            }
+        },
+        props: {
+            items: {
+                type: Array,
+                default: () => []
+            },
+            itemKey: {
+                type: String,
+                default: () => "value"
+            },
+            type: {
+                type: String,
+                default: () => "item"
+            }
+        },
+        computed: {
+            allContacts: function() {
+                return this.items;
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
